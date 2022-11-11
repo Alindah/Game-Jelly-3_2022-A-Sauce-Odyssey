e6248e755e20d22e4b63348f3d342d832a5742b6
Alinda <Alindah@users.noreply.github.com>
2022-11-11 14:09:09 -0500

    Mike's homework
    Implemented player UP movement.
    Mike - implement player DOWN movement.

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+/* This Entity class represents "organic" characters,
+ * including the player and the enemies
+ */
+public class Entity : MonoBehaviour
+{
+    public float speed;
+    public int health;
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : Entity
+{
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        // Set keyboard
+        Keyboard kb = Keyboard.current;
+
+        // Return if no keyboard detected
+        if (kb == null)
+            return;
+
+        // If user presses UP arrow or W key, player moves up
+        if (kb.upArrowKey.IsPressed() || kb.wKey.IsPressed())
+            transform.position = new Vector2(transform.position.x, transform.position.y + speed * Time.deltaTime);
+
+        // If user presses DOWN arrow or S key, player moves down
+        // MIKE - IMPLEMENT CODE
+    }
+}
