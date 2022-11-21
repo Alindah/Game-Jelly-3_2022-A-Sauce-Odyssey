4d859a61f49f54707f6c391155c0ff95bdbb91ea
Mike Morton <mmorton7295@gmail.com>
2022-11-21 14:51:17 -0600

    Changes enemy to be a trigger

--- Assets/Scripts/OnCollision.cs
+++ Assets/Scripts/OnCollision.cs
@@ -9,7 +9,7 @@ public class OnCollision : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
