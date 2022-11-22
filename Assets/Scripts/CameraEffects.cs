849019f74952c0b1eeb465106c9faab070dca12c
Angel <adavila0703@gmail.com>
2022-11-21 19:03:51 -0600

    addressing comments

--- Assets/Scripts/CameraEffects.cs
+++ Assets/Scripts/CameraEffects.cs
@@ -1,68 +1,35 @@
-using System.Collections;
 using UnityEngine;
 using System.Threading.Tasks;
 
 public class CameraEffects : MonoBehaviour
 {
-  private static float magnitude;
-
-  public enum Magnitude
-  {
-    Soft,
-    Hard,
-    Extreme,
-  }
-
-  private static void SetMagnitude(Magnitude magnitude)
-  {
-    switch (magnitude)
+    private static Vector3 RandomCameraPosition()
     {
-      case Magnitude.Soft:
-        CameraEffects.magnitude = .1f;
-        break;
-
-      case Magnitude.Hard:
-        CameraEffects.magnitude = 2f;
-        break;
-
-      case Magnitude.Extreme:
-        CameraEffects.magnitude = 10f;
-        break;
-
-      default:
-        break;
-    }
-  }
-
-  private static Vector3 RandomCameraPosition()
-  {
-    int randomX = Random.Range(-2, 2);
-    int randomY = Random.Range(-2, 2);
-    return new Vector3(randomX, randomY, -10);
-  }
-
-  public static async void ShakeCamera(Camera camera, float duration, Magnitude magnitude, Vector3 initialCameraPos)
-  {
-    Quaternion cameraRotation = camera.transform.rotation;
-
-    // set magnitude 
-    SetMagnitude(magnitude);
-    float endTime = Time.time + duration;
-
-    while (Time.time < endTime)
-    {
-      Vector3 randomCameraPos = RandomCameraPosition();
-
-      // lerp camera
-      Vector3 newCameraPos = Vector3.Lerp(camera.transform.position, randomCameraPos, CameraEffects.magnitude);
-
-      // set new camera pos
-      camera.transform.SetPositionAndRotation(newCameraPos, cameraRotation);
-      await Task.Yield();
+        int randomX = Random.Range(-2, 2);
+        int randomY = Random.Range(-2, 2);
+        return new Vector3(randomX, randomY, -10);
     }
 
-    // reset camera to its original position
-    camera.transform.SetPositionAndRotation(initialCameraPos, cameraRotation);
-  }
+    public static async void ShakeCamera(Camera camera, float duration, float magnitude, Vector3 initialCameraPos)
+    {
+        Quaternion cameraRotation = camera.transform.rotation;
+
+        float endTime = Time.time + duration;
+
+        while (Time.time < endTime)
+        {
+            Vector3 randomCameraPos = RandomCameraPosition();
+
+            // lerp camera
+            Vector3 newCameraPos = Vector3.Lerp(camera.transform.position, randomCameraPos, magnitude);
+
+            // set new camera pos
+            camera.transform.SetPositionAndRotation(newCameraPos, cameraRotation);
+            await Task.Yield();
+        }
+
+        // reset camera to its original position
+        camera.transform.SetPositionAndRotation(initialCameraPos, cameraRotation);
+    }
 
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,33 +3,32 @@ using UnityEngine.InputSystem;
 
 public class Player : Entity
 {
-  private void FixedUpdate()
-  {
-    MovePlayer();
-  }
-
-  private void MovePlayer()
-  {
-    // Set keyboard
-    Keyboard kb = Keyboard.current;
-
-    // Return if no keyboard detected
-    if (kb == null)
-      return;
-
-    // If user presses UP arrow or W key, player moves up
-    if (kb.upArrowKey.IsPressed() || kb.wKey.IsPressed())
-      transform.position = new Vector2(transform.position.x, transform.position.y + speed * Time.deltaTime);
-
-    // If user presses DOWN arrow or S key, player moves down
-    if (kb.downArrowKey.IsPressed() || kb.sKey.IsPressed())
-      transform.position = new Vector2(transform.position.x, transform.position.y - speed * Time.deltaTime);
-
-    if (kb.numpad0Key.IsPressed())
+    private void FixedUpdate()
     {
-      Vector3 initialCameraPos = new Vector3(0.0f, 0.0f, -10.0f);
-      CameraEffects.ShakeCamera(Camera.main, 0.5f, CameraEffects.Magnitude.Soft, initialCameraPos);
-
+        MovePlayer();
     }
-  }
+
+    private void MovePlayer()
+    {
+        // Set keyboard
+        Keyboard kb = Keyboard.current;
+
+        // Return if no keyboard detected
+        if (kb == null)
+            return;
+
+        // If user presses UP arrow or W key, player moves up
+        if (kb.upArrowKey.IsPressed() || kb.wKey.IsPressed())
+            transform.position = new Vector2(transform.position.x, transform.position.y + speed * Time.deltaTime);
+
+        // If user presses DOWN arrow or S key, player moves down
+        if (kb.downArrowKey.IsPressed() || kb.sKey.IsPressed())
+            transform.position = new Vector2(transform.position.x, transform.position.y - speed * Time.deltaTime);
+
+        if (kb.oKey.IsPressed())
+        {
+            Vector3 initialCameraPos = new Vector3(0.0f, 0.0f, -10.0f);
+            CameraEffects.ShakeCamera(Camera.main, 0.5f, 0.1f, initialCameraPos);
+        }
+    }
 }
